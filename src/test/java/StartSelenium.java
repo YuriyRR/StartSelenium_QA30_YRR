cbf5b0e36ba48b3affdc99adde48b549b7ad5781
YuriyRR <yuriy.rah@gmail.com>
2021-10-15 19:40:35 +0300

    commit again

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -31,15 +31,27 @@ public class StartSelenium {
 
         wd.findElement(By.className("container"));
         wd.findElement(By.className("navbar-component_nav__1X_4m"));
-        wd.findElement(By.className(""));
+        wd.findElement(By.className("about_main__2Uv5W"));
+        wd.findElement(By.className("about_logo__1Y8cQ"));
 
-        wd.findElement(By.id(""));
 
-        wd.findElement(By.tagName(""));
+        wd.findElement(By.id("root"));
+
+        wd.findElement(By.tagName("body"));
+        wd.findElement(By.tagName("div"));
+        wd.findElement(By.tagName("h1"));
+        wd.findElement(By.tagName("a"));
+        wd.findElement(By.tagName("h2"));
+        wd.findElement(By.tagName("h3"));
+        wd.findElement(By.tagName("p"));
+        wd.findElement(By.tagName("br"));
+
 
         wd.findElement(By.linkText("HOME"));
 
         wd.findElement(By.partialLinkText("HO"));
+        wd.findElement(By.partialLinkText("AB"));
+        wd.findElement(By.partialLinkText("LO"));
 
 
         //click Login button
